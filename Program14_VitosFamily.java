3b5b3b7b606e2c767549e47bbd9d1cc845f18628
Ganesh K. <kamdiganesh123@gmail.com>
2025-06-23 07:57:21 +0530

    Update Program14_VitosFamily.java
    Signed-off-by: Ganesh K. <kamdiganesh123@gmail.com>

--- Program14_VitosFamily.java
+++ Program14_VitosFamily.java
@@ -1,37 +1,27 @@
 import java.util.*;
 
-public class Program14_VitosFamily {
+public class Program14_VitosFamilyimport {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Read number of test cases
-        int testCases = Integer.parseInt(scanner.nextLine());
-
-        for (int t = 0; t < testCases; t++) {
-            // Read the line and split into tokens
-            String[] input = scanner.nextLine().split(" ");
-            int r = Integer.parseInt(input[0]); // Number of relatives
-
-            // Read street numbers
+        Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
+        
+        while (testCases-- > 0) {
+            int r = sc.nextInt();
             int[] streets = new int[r];
+            
             for (int i = 0; i < r; i++) {
-                streets[i] = Integer.parseInt(input[i + 1]);
+                streets[i] = sc.nextInt();
             }
-
-            // Sort to find the median
+            
             Arrays.sort(streets);
-            int median = streets[r / 2]; // Median minimizes total absolute distance
-
-            // Calculate total distance to the median
+            int median = streets[r / 2];
             int totalDistance = 0;
-            for (int street : streets) {
-                totalDistance += Math.abs(street - median);
+            
+            for (int i = 0; i < r; i++) {
+                totalDistance += Math.abs(streets[i] - median);
             }
-
-            // Output the result
+            
             System.out.println(totalDistance);
         }
-
-        scanner.close();
     }
 }
